ecf7e4bf571ddda1f9c09726304a3ec5ffa66a13
andd54 <anderprima54@gmail.com>
2022-11-06 17:50:11 -0600

    Controller Added

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
