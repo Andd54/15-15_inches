8baecb0d6d0879747ace866c21c6772da406a5b2
Ziyi Wang <zw67@illinois.edu>
2023-02-07 00:44:15 -0600

    fixed few annotations

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\neil                                             */
+/*    Author:       IRobotics VEXU team                                       */
 /*    Created:      Sun Nov 06 2022                                           */
 /*    Description:  V5 project                                                */
 /*                                                                            */
@@ -9,17 +9,13 @@
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// BaseRF               motor         1               
-// BaseRM               motor         2               
+// [15 inches]               [Type]        [Port(s)]
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 //include the autonomous 
 #include "vex.h"
 #include<autonomous.h>
 using namespace vex;
-<<<<<<< HEAD
 competition Competition;
 void autonomous(void){
   //insert function in autonomous.h here
@@ -39,12 +35,4 @@ int main() {
   while(true){
     wait(100,msec);
   }
-=======
-
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  //fun update yay wow
-  
->>>>>>> b30dd73b003f2725b6e2370e4118cc5d282bbb3d
 }
