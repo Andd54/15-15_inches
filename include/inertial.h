1bcd578d44a0513dc08970e5aec0af5adc5bb5e4
ai-manoj <mnj.aishwarya@gmail.com>
2023-02-22 21:08:39 -0600

    found velocity

---
+++ include/inertial.h
@@ -0,0 +1,27 @@
+#include <basicFunctions.h>
+
+double facing; 
+double yaw_angle;
+double x_accel;
+double y_accel;
+double x_velocity;
+double y_velocity;
+
+
+
+void inertial() {
+  //we want position (theta), direction we're facing, and velocity(?)
+  while(true)
+  {
+    facing = iner.heading(deg); //gets yaw heading 0-360
+    yaw_angle = iner.rotation(deg); 
+    x_accel = iner.acceleration(xaxis);
+    y_accel = iner.acceleration(yaxis);
+    double x_accel_inch = acceleration_convertor(x_accel);
+    double y_accel_inch = acceleration_convertor(y_accel);
+    x_velocity += x_accel_inch*(0.02);
+    y_velocity += y_accel_inch*(0.02);
+    vex::task::sleep(20);
+  }
+
+}
