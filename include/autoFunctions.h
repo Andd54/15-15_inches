700ff608e8addb53d90d831eaeca9ded89e7de9a
Ziyi Wang <zw67@illinois.edu>
2023-02-22 19:25:45 -0600

    autofunction updated

--- include/autoFunctions.h
+++ include/autoFunctions.h
@@ -7,3 +7,33 @@ void drive_distance(double distance, double time) { // take in distance to trave
   drive_right_rpm(wheel_v);
   wait(time,sec);
 }
+
+void turn(double rot_angle){
+  iner.calibrate();
+  double error;
+  double lastError = 0;
+  double integral = 0;
+  double derivative;
+  double power;
+  double kP = 0.8;
+  double kI = 0.1;
+  double kD = 0.1;
+
+  double tolerence = 5;// tolerence for terminating the loop
+
+  while (fabs(error)>tolerence){
+    error = rot_angle - iner.rotation(rotationUnits::deg);
+    integral += error;
+    derivative = error - lastError;
+    power = kP * error + kI * integral + kD * derivative;
+    power = power*50;
+    //change in units
+    //Add related moving mechanism to keep rotate
+    
+    
+    lastError = error;
+    
+    vex::task::sleep(20);
+  }
+}
+
