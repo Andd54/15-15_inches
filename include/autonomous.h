1bb39c14fbad5d7becebbe7239e1bab39f3d19e7
Neil Ghose <neil@ghose.us>
2023-03-01 21:52:34 -0600

    fixed angles

--- include/autonomous.h
+++ include/autonomous.h
@@ -1,7 +1,8 @@
 #include<driveControl.h>
 //shoot all the matchloads
 double shooting_radius = 53;
-double angles [] = {-71.0,47.0,-95.0,-27.0,61.0,-25.0,-35.0,-90.0,90.0,-90.0};
+double angles [] = {-71.0, 47.0, 43, -86, -27.0, 61.0, -25.0, -35.0, 45, -90, 90.0, -90.0, 7.0, -71.0, 47.0, 43, -86, -27.0, 61.0, -10, 10, -10, 10, -31,-76};
+
 double drive_dis[]={39, -21, 22, 34, -12, 48, 16, -13, -24, 30, 3*tile_width};
 void autonomous(){
   int drive_index = 0;
